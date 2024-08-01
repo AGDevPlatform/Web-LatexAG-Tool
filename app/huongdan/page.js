54115d0f6749637c8e04d18063eb05105b9bfa46
vi nguyen <nguyenduongthevi31415@gmail.com>
2024-08-01 19:00:15 +0700

    Update

--- app/huongdan/page.js
+++ app/huongdan/page.js
@@ -60,7 +60,54 @@ export default function HuongDan() {
                   </div>
                 </div>
               </div>
-
+              <div className="bg-gray-100 p-4 rounded-lg">
+                <p className="text-xl font-semibold text-gray-800 mb-2">
+                  2. C&acirc;u hỏi trắc nghiệm đúng sai
+                </p>
+                <div className="text-gray-600">
+                  <Image
+                    className="mb-2"
+                    src={"/HD2.png"}
+                    alt="HD1"
+                    width={1000}
+                    height={100}
+                  />
+                  <p className="mb-5 font-bold"> 👉 Quy tắc c&acirc;u hỏi</p>
+                  <div className="p-0">
+                    <div className="grid grid-cols-1 gap-2 ">
+                      <p>
+                        - C&acirc;u hỏi phải bắt đầu bằng chữ{" "}
+                        <strong>&quot;C&acirc;u&quot;</strong>. V&iacute; dụ:{" "}
+                        <strong>&quot;C&acirc;u 1.&quot;</strong>,{" "}
+                        <strong>&quot;C&acirc;u 2:&quot;</strong>
+                        ,...
+                      </p>
+                      <p>
+                        - C&aacute;c đ&aacute;p &aacute;n bắt buộc phải được bắt
+                        đầu bằng{" "}
+                        <strong>
+                          {" "}
+                          &quot;a)&quot;, &quot;b)&quot;, &quot;c)&quot;,
+                          &quot;d)&quot;
+                        </strong>
+                        .
+                      </p>
+                      <p>
+                        - Đ&aacute;p &aacute;n n&agrave;o đ&uacute;ng th&igrave;
+                        điền dấu <strong>#</strong> ở đầu nội dung của
+                        đ&aacute;p &aacute;n đ&oacute;. V&iacute; dụ:{" "}
+                        <strong>a)</strong>
+                        #Đ&aacute;p &aacute;n 1,...
+                      </p>
+                      <p>
+                        - Lời giải bắt đầu bằng chữ{" "}
+                        <strong>&quot;Lời giải&quot;</strong> đặt ở cuối
+                        c&ugrave;ng c&acirc;u hỏi.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div className="bg-gray-50 px-6 py-4">
                 <p className="text-center text-gray-500 text-sm">
                   © 2024 Công ty của bạn. Tất cả các quyền được bảo lưu.
